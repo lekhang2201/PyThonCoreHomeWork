59eeaa403aef73d9efc510789c956893d3c9fdc8
Le Khang <lekhang2201997@gmail.com>
2020-08-27 20:58:44 +0700

    Submit Test

--- MidTestPython/Task3.py
+++ MidTestPython/Task3.py
@@ -2,9 +2,11 @@ import math
 
 
 def isPrime(n):
+    if n < 2:
+        return False
     if n == 2 or n == 3:
         return True
-    for i in range(2, int(math.sqrt(n))):
+    for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
             return False
     return True
